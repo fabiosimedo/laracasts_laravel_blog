d979a3134ba86aeceb1591844ca3c037e28ba79c
ASPIREFabio <fabiosimedo@hotmail.com>
2023-08-15 21:49:45 -0300

    Added google tag manager js in head tag in layout.blade file

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -1,6 +1,15 @@
 <!doctype html>
 
 <head>
+    <!-- Google Tag Manager -->
+    <script>
+        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-WNKXVDQ9');
+    </script>
+    <!-- End Google Tag Manager -->
 
     <!-- Google tag (gtag.js) -->
     <script async src="https://www.googletagmanager.com/gtag/js?id=G-2JDEZED7FH"></script>
@@ -40,6 +49,10 @@
 
 </head>
 <body style="font-family: Open Sans, sans-serif">
+    <!-- Google Tag Manager (noscript) -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WNKXVDQ9"
+    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
     <x-flash />
 
     <section>
